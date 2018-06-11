a4c0855206e75f0b5b39c2fd3cf147f33cfd756b
Jonathan Courat <jonathan.courat@campus-numerique-in-the-alps.com>
2018-06-11 15:15:08 +0200

    Fonction construction du plateau + click getId()

--- taquin.js
+++ taquin.js
@@ -1,3 +1,29 @@
-$(document).ready(function(){
-}
-console.log('');
+$(document).ready( function(){
+    console.log("Hi there this is jQuery");
+    
+    //-- Construction du plateau
+    let nbCasesX = 4;
+    let nbCasesY = 4;
+    let rowNum = 0;
+    let caseIndex = 0;
+    let userValue = 1;
+
+    for(let y = 0; y < nbCasesY; y++) {
+        $("#plateau").append('<div id="row'+ rowNum +'" class="row"></div>');
+        for(let x = 0; x < nbCasesX; x ++) {
+            $("#row" + rowNum).append('<div id="'+ caseIndex + '"class="col-lg-2 case">'+ userValue + '</div>');
+            caseIndex++;
+            userValue++;
+        }
+        rowNum++;
+    }
+
+
+    //-- Click
+    $(".case").click(function() {
+        console.log($(this).attr('id'));
+    });
+
+})
+
+console.log("") ;
