5a01802762c5f420cb7bf789029c0e38d25032fa
EDG\olivier.menuel <olivier.menuel@campus-numerique-in-the-alps.com>
2018-06-11 15:19:02 +0200

    remplir plateau

--- taquin.js
+++ taquin.js
@@ -1,3 +1,28 @@
 $(document).ready(function(){
+    rempli_plateau();
+    affiche_tableau();
+});
+var n = 4;
+
+var plateau = [];
+function rempli_plateau() {
+    for(var i=0; i<=n; i++)
+        for(var j=0; j<=n; j++)
+            plateau [i][j] = i*j+i;
 }
-console.log('');
+
+
+
+
+function affiche_tableau() {
+    for(var i=0; i<n; i++)
+        for(var j=0; j<n; j++)
+            console.log("Case "+ i + "-" + j +" : "+ plateau[i][j]);
+}
+
+
+
+
+
+
+
