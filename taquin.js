117b2536128f604317a30e81d0ac8f96b20c156d
Astrid Bossy <astrid.bossy@Digital-Grenoble.local>
2018-06-11 16:45:18 +0200

    function createBoardGame + shuffle

--- taquin.js
+++ taquin.js
@@ -2,18 +2,20 @@ $(document).ready( function(){
     console.log("Hi there this is jQuery");
     
     //-- Construction du plateau
-    let nbCasesX = 4;
-    let nbCasesY = 4;
+
+    let nbCasesX = 6;
+    let nbCasesY = 6;
     let rowNum = 0;
     let caseIndex = 0;
-    let userValue = 1;
+    let boardItems = Array.from(Array(nbCasesX*nbCasesY).keys());// permet de créer un tableau en prenant les valeurs de X et Y.
+    let plateauInitial = createBoardGame(boardItems, nbCasesY, nbCasesX)
 
     for(let y = 0; y < nbCasesY; y++) {
         $("#plateau").append('<div id="row'+ rowNum +'" class="row"></div>');
         for(let x = 0; x < nbCasesX; x ++) {
-            $("#row" + rowNum).append('<div id="'+ caseIndex + '"class="col-lg-2 case">'+ userValue + '</div>');
+            $("#row" + rowNum).append('<div id="'+ caseIndex + '"class="col-lg-2 case">'+ plateauInitial[y][x] + '</div>');
             caseIndex++;
-            userValue++;
+            
         }
         rowNum++;
     }
@@ -23,6 +25,31 @@ $(document).ready( function(){
     $(".case").click(function() {
         console.log($(this).attr('id'));
     });
+
+
+    //--Fonction shuffle
+    function shuffle(a){
+        for (let i=a.length -1;i>0; i--){
+            const j = Math.floor(Math.random()*(i+1));
+            [a[i], a[j]] = [a[j], a[i]];
+        }
+        return a;
+    }
+
+    //-- Fonction qui permet de retourner un tableau au format [[],[],[]....]
+    function createBoardGame(tab,nbY, nbX){
+        let board = []; // on definit un plateau vide pour le moment
+        let index = 0; // on definit une variable index qui represente la valeur à assigner.
+        for(let y = 0; y < nbY; y++){ // y represente les lignes du board.
+            let xCase = []; // le tableau va contenir l'index des cases.
+            for (let x = 0; x < nbX; x++){
+                xCase.push(tab[index]); // on pousse les données dans le tableau.
+                index ++;
+            }
+            board.push(xCase);
+        }
+        return board;
+    }
 
 })
 
