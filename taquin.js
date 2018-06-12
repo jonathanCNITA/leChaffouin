efa085fae5443f3719bfc36a2e823768db3678ad
Jonathan Courat <jonathan.courat@campus-numerique-in-the-alps.com>
2018-06-12 10:19:28 +0200

    Clean js front file

--- taquin.js
+++ taquin.js
@@ -18,26 +18,8 @@ $(document).ready( function(){
             
         }
         rowNum++;
-        rempli_plateau();
-        affiche_tableau();
     }
-    var n = 4;
-
-    var plateau = [];
-    function rempli_plateau() {
-        for(var i=0; i<=n; i++)
-            for(var j=0; j<=n; j++)
-                plateau [i][j] = i*j+i;
-    }
-
-
-    function affiche_tableau() {
-        for(var i=0; i<n; i++)
-            for(var j=0; j<n; j++)
-                console.log("Case "+ i + "-" + j +" : "+ plateau[i][j]);
-    }
-
-
+   
     //-- Click
     $(".case").click(function() {
         console.log($(this).attr('id'));
